e8a2bcdf45d3d2ce6a133f4f68785767586639df
Alexey Kozhevnikov <nezumikun@yandex.ru>
2025-08-01 21:33:32 +0300

    Рука отображается по кнопке

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
   const hand = ref<Hand>(new Hand())
   const tenhouHandRequest = ref<TenhouHandRequest>()
-  await getNextGameResult();
   
   async function getNextGameResult() {
     const fetchData = await $fetch<TenhouHandRequest>('/api/getRandomTehnouHand')
@@ -16,93 +15,84 @@
 
 <template>
   <div class="p-10">
-    <h1 class="text-3xl text-center">
-      {{ hand.GUID }}
-    </h1>
-    <!--
-    <div>
-    handArray {{ JSON.stringify(handArray) }}
-    </div>
-    <div>
-    melds {{ JSON.stringify(melds) }}
-    </div>
-    <div>
-    winningTile {{ JSON.stringify(winningTile) }}
-    </div>
-    -->
-    <div class="text-center w-full">
-      <div class="hand">
-        <template v-for="tile in hand.hand" :key="tile">
-          <img :src="tile.getImageName()" :class="tile.getCssClasses()">
-        </template>
-      </div>
-      <div class="tile-winning">
-        <img :src="hand.winningTile.getImageName()" class="tile tile-winning">
-      </div>
-      <div class="melds">
-        <template v-for="meld in hand.melds">
-          <template v-for="tile in meld" :key="tile">
+    <template v-if="hand.hand.length">
+      <h1 class="text-3xl text-center">
+        {{ hand.GUID }}
+      </h1>
+      <div class="text-center w-full">
+        <div class="hand">
+          <template v-for="tile in hand.hand" :key="tile">
             <img :src="tile.getImageName()" :class="tile.getCssClasses()">
           </template>
-        </template>
-      </div>
-    </div>
-    <div class="winning-parameters grid grid-cols-5 pt-8 w-fit min-w-1/2 gap-4 mx-auto">
-      <div>{{ $t("wind_of_round") }}</div>
-      <div>{{ $t("wind_of_seat") }}</div>
-      <div>{{ $t("dora_indicator") }}</div>
-      <div>{{ $t("uradora_indicator") }}</div>
-      <div>{{ $t("winning_parameters") }}</div>
-      <div><img :src="hand.roundWind.getImageName()" :class="hand.roundWind.getCssClasses()"></div>
-      <div><img :src="hand.seatWind.getImageName()" :class="hand.seatWind.getCssClasses()"></div>
-      <div>
-        <template v-for="doraIndicator in hand.doraIndicators" :key="doraIndicator">
-          <img :src="doraIndicator.getImageName()" :class="doraIndicator.getCssClasses()">
-        </template>
-      </div>
-      <div>
-        <template v-if="hand.uraDoraIndicators">
-          <template v-for="uraDoraIndicator in hand.uraDoraIndicators" :key="uraDoraIndicator">
-            <img :src="uraDoraIndicator.getImageName()" :class="uraDoraIndicator.getCssClasses()">
+        </div>
+        <div class="tile-winning">
+          <img :src="hand.winningTile.getImageName()" class="tile tile-winning">
+        </div>
+        <div class="melds">
+          <template v-for="meld in hand.melds">
+            <template v-for="tile in meld" :key="tile">
+              <img :src="tile.getImageName()" :class="tile.getCssClasses()">
+            </template>
           </template>
-        </template>
-        <template v-if="!hand.uraDoraIndicators.length">
-          {{ $t("no") }}
-        </template>
-      </div>
-      <div>
-        <div class="grid grid-cols-1">
-          <div>{{ $t("riichi_option") }}: {{ hand.isRiichi ? $t("yes") : $t("no") }}</div>
-          <div>{{ $t("tsumo_option") }}: {{ hand.isTsumo ? $t("yes") : $t("no") }}</div>
         </div>
       </div>
-    </div>
-    <div class="winning-details grid grid-cols-2 pt-8 w-fit min-w-1/2 gap-4 mx-auto">
-      <div>{{ $t("Yaku") }}</div>
-      <div>{{ $t("Fu") }}</div>
-      <div>
-        <div class="grid grid-cols-2">
-          <template v-for="yaku in hand.yaku" :key="yaku">
-            <div>{{ $t(yaku.codeName) }}</div>
-            <div class=" text-right">{{ yaku.price }}</div>
+      <div class="winning-parameters grid grid-cols-5 pt-8 w-fit min-w-1/2 gap-4 mx-auto">
+        <div>{{ $t("wind_of_round") }}</div>
+        <div>{{ $t("wind_of_seat") }}</div>
+        <div>{{ $t("dora_indicator") }}</div>
+        <div>{{ $t("uradora_indicator") }}</div>
+        <div>{{ $t("winning_parameters") }}</div>
+        <div><img :src="hand.roundWind.getImageName()" :class="hand.roundWind.getCssClasses()"></div>
+        <div><img :src="hand.seatWind.getImageName()" :class="hand.seatWind.getCssClasses()"></div>
+        <div>
+          <template v-for="doraIndicator in hand.doraIndicators" :key="doraIndicator">
+            <img :src="doraIndicator.getImageName()" :class="doraIndicator.getCssClasses()">
           </template>
-          <div class="winning-details-summary col-span-2 text-right">
-            {{ hand.han  }}
+        </div>
+        <div>
+          <template v-if="hand.uraDoraIndicators">
+            <template v-for="uraDoraIndicator in hand.uraDoraIndicators" :key="uraDoraIndicator">
+              <img :src="uraDoraIndicator.getImageName()" :class="uraDoraIndicator.getCssClasses()">
+            </template>
+          </template>
+          <template v-if="!hand.uraDoraIndicators.length">
+            {{ $t("no") }}
+          </template>
+        </div>
+        <div>
+          <div class="grid grid-cols-1">
+            <div>{{ $t("riichi_option") }}: {{ hand.isRiichi ? $t("yes") : $t("no") }}</div>
+            <div>{{ $t("tsumo_option") }}: {{ hand.isTsumo ? $t("yes") : $t("no") }}</div>
           </div>
         </div>
       </div>
-      <div>
-        <div class="grid grid-cols-5">
-          <template v-for="fu in hand.fuDetails" :key="fu">
-            <div class="col-span-4">{{ $t('fu_' + fu.reason) }}</div>
-            <div class=" text-right">{{ fu.fu }}</div>
-          </template>
-          <div class="winning-details-summary col-span-5 text-right">
-            {{ hand.fu  }}
+      <div class="winning-details grid grid-cols-2 pt-8 w-fit min-w-1/2 gap-4 mx-auto">
+        <div>{{ $t("Yaku") }}</div>
+        <div>{{ $t("Fu") }}</div>
+        <div>
+          <div class="grid grid-cols-2">
+            <template v-for="yaku in hand.yaku" :key="yaku">
+              <div>{{ $t(yaku.codeName) }}</div>
+              <div class=" text-right">{{ yaku.price }}</div>
+            </template>
+            <div class="winning-details-summary col-span-2 text-right">
+              {{ hand.han  }}
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="grid grid-cols-5">
+            <template v-for="fu in hand.fuDetails" :key="fu">
+              <div class="col-span-4">{{ $t('fu_' + fu.reason) }}</div>
+              <div class=" text-right">{{ fu.fu }}</div>
+            </template>
+            <div class="winning-details-summary col-span-5 text-right">
+              {{ hand.fu  }}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </template>
     <div class="text-center w-full pt-4">
       <UButton loading-auto color="secondary" @click="getNextGameResult()">{{ $t('next_hand') }}</UButton>
     </div>
